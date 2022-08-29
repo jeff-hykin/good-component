a33e80ca802525827e05549dd9d2b638fbfa2630
Jeff Hykin <jeff.hykin@gmail.com>
2022-08-29 08:43:06 -0500

    -

--- loader.js
+++ loader.js
@@ -1,6 +1,6 @@
 // put this in <body>
 `
-<div style="display: flex;justify-content: center;align-items: center;height: 100%;">
+<div style="display: flex;justify-content: center;align-items: center;height: 100vh;">
     <div style="width: 50px;height: 50px;border: 10px solid #dddddd;border-top-color: #009579;border-radius: 50%;transform: rotate(0.16turn);" id="good-component--initial-loader">
     </div>
 </div>
